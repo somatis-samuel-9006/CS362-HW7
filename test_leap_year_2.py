a17f5cc3e5fa1e803a4a283fb2c064f754406cb3
Samuel <ssg5689@gmail.com>
2021-03-03 23:03:32 -0800

    wrote third test for numbers divisible by 4, 100, and 400, including test for numbers divisible by 4 and 100, but not 400. this test is currently failing, all other tests are working

--- test_leap_year_2.py
+++ test_leap_year_2.py
@@ -10,7 +10,10 @@ class testLeap(unittest.TestCase):
     def test2(self):
         self.assertEqual(leap_year_2.leap_year(400), "That is a leap year")
         self.assertEqual(leap_year_2.leap_year(12), "That is not a leap year")
-
+    #divisible by 4 and 100 and 400. includes test for div by 4 and 100, but not by 400
+    def test3(self):
+        self.assertEqual(leap_year_2.leap_year(400), "That is a leap year")
+        self.assertEqual(leap_year_2.leap_year(200), "That is not a leap year")
 
 if __name__ == '__main__':
     unittest.main()
