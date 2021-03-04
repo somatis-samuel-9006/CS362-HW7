27ad632cc2ca296e1f6d83a8bfb5a03196c2255b
Samuel <ssg5689@gmail.com>
2021-03-03 22:54:07 -0800

    wrote second test for years divisible by 4 and 100, including atest for numbers divisible by 4 but not by 100. currently failing

--- test_leap_year_2.py
+++ test_leap_year_2.py
@@ -2,9 +2,14 @@ import leap_year_2
 import unittest
 
 class testLeap(unittest.TestCase):
+    #divisible by 4 or not
     def test1(self):
         self.assertEqual(leap_year_2.leap_year(400), "That is a leap year")
         self.assertEqual(leap_year_2.leap_year(401), "That is not a leap year")
+    #divisible by 4 and 100. includes test for div by 4 by not by 100
+    def test2(self):
+        self.assertEqual(leap_year_2.leap_year(400), "That is a leap year")
+        self.assertEqual(leap_year_2.leap_year(12), "That is not a leap year")
 
 
 if __name__ == '__main__':
