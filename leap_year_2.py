ee39cf0bd29a449d191bce78d25ea1d9995af11c
Samuel <ssg5689@gmail.com>
2021-03-03 23:16:33 -0800

    wrote code to fully complete the program specification for years divisible by 4 except years divisible by 100 unless they are divisible by 400

--- leap_year_2.py
+++ leap_year_2.py
@@ -1,9 +1,12 @@
 def leap_year(year):
     if(year % 4 == 0):
         if(year % 100 == 0):
-            return "That is a leap year"
+            if(year % 400 == 0):
+                return "That is a leap year"
+            else:
+                return "That is not a leap year"
         else:
-            return "That is not a leap year"
+            return "That is a leap year"
     else:
         return "That is not a leap year"
 
