4d5346109da5a3888594c1a9048a40ed6c08bee4
Samuel <ssg5689@gmail.com>
2021-03-03 22:58:08 -0800

    wrote just enough code to get tests for numbers divisible by 100 and 4, but not just 4 or just 100, to pass. all tests in test suite are currently passing

--- leap_year_2.py
+++ leap_year_2.py
@@ -1,6 +1,9 @@
 def leap_year(year):
     if(year % 4 == 0):
-        return "That is a leap year"
+        if(year % 100 == 0):
+            return "That is a leap year"
+        else:
+            return "That is not a leap year"
     else:
         return "That is not a leap year"
 
