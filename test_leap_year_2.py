d3f2530eea44ed52ac2165a30544aee93387ee03
Samuel <ssg5689@gmail.com>
2021-03-03 23:14:36 -0800

    fixed error on line 12, years divisible by 4 but not 100 meet the criteria for being a leap year

--- test_leap_year_2.py
+++ test_leap_year_2.py
@@ -9,7 +9,7 @@ class testLeap(unittest.TestCase):
     #divisible by 4 and 100. includes test for div by 4 by not by 100
     def test2(self):
         self.assertEqual(leap_year_2.leap_year(400), "That is a leap year")
-        self.assertEqual(leap_year_2.leap_year(12), "That is not a leap year")
+        self.assertEqual(leap_year_2.leap_year(12), "That is a leap year")
     #divisible by 4 and 100 and 400. includes test for div by 4 and 100, but not by 400
     def test3(self):
         self.assertEqual(leap_year_2.leap_year(400), "That is a leap year")
